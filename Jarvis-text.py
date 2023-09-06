82e9f2afc2d37f0c5fd33dab7117ee006b2a0965
Shivansh Gupta <shivanshgupta0987+github@gmail.com>
2023-09-06 23:24:03 +0530

    added more complex algo so number are saved in encrupted form and done some modification in database also

--- Jarvis-text.py
+++ Jarvis-text.py
@@ -16,6 +16,7 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+# Function to wish
 def wishMe():
     hour = int(datetime.datetime.now().hour)
     if hour >=0 and hour<12:
@@ -26,30 +27,62 @@ def wishMe():
         speak("good evening!")
     speak("i am Jarvis , please tell how can i help you ? ")
 
+# Function to encode a phone number
+def encode_number(number):
+    encoded_number = ""
+    for char in number:
+        encoded_number += chr(ord(char) + 1)
+    return encoded_number
+
+# Function to decode an encoded phone number
+def decode_number(encoded_number):
+    decoded_number = ""
+    for char in encoded_number:
+        decoded_number += chr(ord(char) - 1)
+    return decoded_number
+
+# Function to load recipients from an encrypted file
+def load_recipients(filename):
+    recipients = {}
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                name, encoded_number = line.strip().split(': ')
+                number = decode_number(encoded_number)
+                recipients[name] = number
+        return recipients
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+        return {}
+
+# Function to save recipients to an encrypted file
+def save_recipients(filename, recipients):
+    with open(filename, 'w') as file:
+        for name, number in recipients.items():
+            encoded_number = encode_number(number)
+            file.write(f"{name}: {encoded_number}\n")
+
+# Function to send a WhatsApp message
 def send_message_to_whatsapp():
-    recipient_number = input("Enter the recipient's phone number (including country code): ")
+    recipients = load_recipients("recipients.txt")
+
+    recipient_name = input("Enter the recipient's name: ")
     message = input("Enter the message you want to send: ")
     scheduled_time = input("Enter the time to send the message (HH:MM): ")
 
-    try:
-        # Parse the scheduled time
-        scheduled_hour, scheduled_minute = map(int, scheduled_time.split(':'))
+    if recipient_name in recipients:
+        recipient_number = recipients[recipient_name]
 
-        # Calculate the time difference in minutes
-        current_time = datetime.datetime.now()
-        scheduled_time = datetime.datetime(current_time.year, current_time.month, current_time.day, scheduled_hour, scheduled_minute)
+        try:
+            # Parse the scheduled time
+            scheduled_hour, scheduled_minute = map(int, scheduled_time.split(':'))
 
-        time_difference = scheduled_time - current_time
-        minutes_until_scheduled_time = time_difference.total_seconds() / 60
-
-        # Check if the scheduled time is in the future
-        if minutes_until_scheduled_time < 0:
-            print("Scheduled time should be in the future.")
-        else:
-            kit.sendwhatmsg(f"+{recipient_number}", message, scheduled_hour, scheduled_minute)
-            print(f"WhatsApp message scheduled to be sent to {recipient_number} at {scheduled_time}: {message}")
-    except Exception as e:
-        print(f"An error occurred while scheduling the WhatsApp message: {str(e)}")
+            kit.sendwhatmsg(recipient_number, message, scheduled_hour, scheduled_minute)
+            print(f"WhatsApp message scheduled to be sent to {recipient_name} at {scheduled_time}: {message}")
+        except Exception as e:
+            print(f"An error occurred while scheduling the WhatsApp message: {str(e)}")
+    else:
+        print(f"Recipient '{recipient_name}' not found in the recipient list.")
 
 if __name__ == "__main__":
     wishMe()
@@ -67,3 +100,10 @@ if __name__ == "__main__":
         elif 'send message on WhatsApp' in main:
             send_message_to_whatsapp()
         
+        elif 'add new contact for whatsapp' in main:
+            recipient_name = input("Enter the recipient's name: ")
+            recipient_number = input("Enter the recipient's phone number (including country code): ")
+            recipients = load_recipients("recipients.txt")
+            recipients[recipient_name] = recipient_number
+            save_recipients("recipients.txt", recipients)
+            print(f"Recipient '{recipient_name}' added successfully.")
